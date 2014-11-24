e7f6e9d8933191fc385c202e5f18f2bb0c9d14f9
Mathilde <mathilde.maltaire@gmail.com>
2014-11-24 14:15:31 +0100

    import

--- src/com/example/projetpdr/MainActivity.java
+++ src/com/example/projetpdr/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.projetpdr;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 
 
